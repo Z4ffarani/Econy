6562a1de014d6b96c4abf140bc559f766f6a2012
Z4ffarani <zaffarani.kaique@gmail.com>
2024-11-22 00:48:41 -0300

    input spacing fix

--- econy/src/pages/Contratos.jsx
+++ econy/src/pages/Contratos.jsx
@@ -24,11 +24,11 @@ export default function Contratos(){
     return (
         <>
             <div className="flex flex-col justify-center items-center">  
-                <div className="flex w-[100%] h-[25px] bg-[#005030] justify-center items-center | sm:hidden">
+                <div className="flex w-[100%] h-[25px] mb-[-19px] sm:mb-0 bg-[#005030] justify-center items-center | sm:hidden">
                     <h1 className="text-[#00C174]">CONTRATOS</h1>
                 </div>
             
-            <div className="scale-95 mt-[-50px] sm:scale-100 sm:mt-[0]">
+            <div className={`scale-95 sm:scale-100`}>
                 <div className="flex flex-col my-[30px] | sm:mt-0">
                 {/* MESMO SISTEMA DE BUSCA DA PAGINA DE EMPRESAS */}
                         <input
@@ -44,10 +44,10 @@ export default function Contratos(){
                                 {   
                         
                                 searchEmpresas.length > 0 ?
-                            
                                     searchEmpresas.map(empresa => (
                                         // include, só exibira contratos caso tenham sido realizados na pagina de empresas
                                         (empresa.id) in IDS ?
+
                                         <ContratoCard   id = {empresa.id}
                                                         empresaImg = {empresa.url} 
                                                         empresaNome = {empresa.nome}
@@ -56,6 +56,7 @@ export default function Contratos(){
                                         : null
                                     ))
                                 :
+
                                 <p className="pt-3">Não há resultados para sua busca.</p>
                             }
                         </div>
